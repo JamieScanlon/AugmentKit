2fc97ec24d7cf05dd58433ef99217dd07606ca6f
Jamie Scanlon <jamie@tenthlettermade.com>
2017-12-09 10:53:59 -0800

    Added a command line tool for serializing models into the AKModel format. The serialization code is working but the CL tool still has problems correctly serializing a scenekit file that is on the file system instead of in the app bundle. To demonstate and test what proper seralization looks like, I also made an iOS app that transfers a sceneKit model from the app bundle to the documents directory and serializes it (correctly)

---
+++ AugmentKitCLTools/ViewController.swift
@@ -0,0 +1,136 @@
+//
+//  ViewController.swift
+//  AugmentKitCLTools
+//
+//  Created by Jamie Scanlon on 12/9/17.
+//  Copyright © 2017 TenthLetterMade. All rights reserved.
+//
+
+import UIKit
+import AugmentKitShader
+import Metal
+import MetalKit
+import ModelIO
+import SceneKit.ModelIO
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var serializeButton: UIButton!
+    @IBOutlet var deserializeButton: UIButton!
+    
+    var documentsDirectory: URL {
+        return FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let originalFileURL = Bundle.main.url(forResource: "Pin", withExtension: "scn") else {
+            print("Pin.scn file not found in bundle")
+            return
+        }
+        
+        guard let originalContents = try? Data(contentsOf: originalFileURL) else {
+            print("Cannot readt the contents of Pin.scn")
+            return
+        }
+        
+        let writableFileURL = documentsDirectory.appendingPathComponent("Pin.scn")
+        
+        print("Copying file: \(originalFileURL.absoluteString) to: \(writableFileURL.absoluteString)")
+        try? originalContents.write(to: writableFileURL, options: .atomic)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func serializeTapped(sender: UIButton) {
+        let url = documentsDirectory.appendingPathComponent("Pin.scn")
+        print("serialize file at path: \(url.absoluteString)")
+        serializeMDLAsset(withURL: url)
+    }
+    
+    @IBAction func deserializeTapped(sender: UIButton) {
+        let url = documentsDirectory.appendingPathComponent("model.dat")
+        print("deserialize file at path: \(url.absoluteString)")
+        deserializeAndPrint(withURL: url)
+    }
+    
+//    fileprivate var model: AKModel?
+    
+    fileprivate func serializeMDLAsset(withURL url: URL) {
+        
+        var error: NSError?
+        let myAsset: MDLAsset? = {
+            if url.pathExtension == "scn" {
+                guard let scene = try? SCNScene(url: url, options: nil) else {
+                    return nil
+                }
+                return MDLAsset(scnScene: scene)
+            } else {
+                return MDLAsset(url: url, vertexDescriptor: AKSimpleModel.newVertexDescriptor(), bufferAllocator: nil, preserveTopology: false, error: &error)
+            }
+        }()
+        
+        guard let asset = myAsset, error == nil else {
+            print("Model file not found. \(url.absoluteString)")
+            return
+        }
+        
+        // Load meshes into the model
+        let model = AKMDLAssetModel(asset: asset)
+        
+        NSKeyedArchiver.archiveRootObject(AKModelCodingWrapper(model: model), toFile: url.deletingLastPathComponent().appendingPathComponent("model.dat").path)
+        
+    }
+    
+    fileprivate func deserializeAndPrint(withURL url: URL) {
+        
+        guard let data = try? Data(contentsOf: url) else {
+            print("File not found. \(url.absoluteString)")
+            return
+        }
+        
+        if let wrapper = NSKeyedUnarchiver.unarchiveObject(with: data) as? AKModelCodingWrapper {
+            
+            guard let archivedModel = wrapper.model else {
+                print("AKModel is empty.")
+                return
+            }
+            
+            print("indexBuffers: \(archivedModel.indexBuffers)")
+            print("instanceCount: \(archivedModel.instanceCount)")
+            print("jointRootID: \(archivedModel.jointRootID)")
+            print("localTransformAnimationIndices: \(archivedModel.localTransformAnimationIndices)")
+            print("localTransformAnimations: \(archivedModel.localTransformAnimations)")
+            print("localTransforms: \(archivedModel.localTransforms)")
+            print("meshes: \(archivedModel.meshes)")
+            print("meshNodeIndices: \(archivedModel.meshNodeIndices)")
+            print("meshSkinIndices: \(archivedModel.meshSkinIndices)")
+            print("nodeNames: \(archivedModel.nodeNames)")
+            print("parentIndices: \(archivedModel.parentIndices)")
+            print("sampleTimes: \(archivedModel.sampleTimes)")
+            print("skeletonAnimations: \(archivedModel.skeletonAnimations)")
+            print("skins: \(archivedModel.skins)")
+            print("texturePaths: \(archivedModel.texturePaths)")
+            print("vertexBuffers: \(archivedModel.vertexBuffers)")
+            print("vertexDescriptors: \(archivedModel.vertexDescriptors)")
+            print("worldTransformAnimationIndices: \(archivedModel.worldTransformAnimationIndices)")
+            print("worldTransformAnimations: \(archivedModel.worldTransformAnimations)")
+            print("worldTransforms: \(archivedModel.worldTransforms)")
+            
+        } else {
+            
+            print("Could not unarchive the model file.")
+            
+        }
+    }
+
+
+}
+
+
+
--- akcltool/AugmentKitCLTools.swift
+++ akcltool/AugmentKitCLTools.swift
@@ -16,12 +16,14 @@ import SceneKit.ModelIO
 enum OptionType: String {
     case serialize = "s"
     case help = "h"
+    case verify = "v"
     case unknown
     
     init(value: String) {
         switch value {
         case "s": self = .serialize
         case "h": self = .help
+        case "v": self = .verify
         default: self = .unknown
         }
     }
@@ -29,9 +31,9 @@ enum OptionType: String {
 
 class AugmentKitCLTools {
     
-    let consoleIO = ConsoleIO()
-    
     func staticMode() {
+        
+        ConsoleIO.writeMessage("WARNING: THIS TOOL IS STILL IN DEVELOPEMNT. There are known problems serializing a file on the filesystem. The generated file may be missing mesh data.", to: .standard)
         
         for argument in CommandLine.arguments[1...] {
             if argument.hasPrefix("-") {
@@ -44,6 +46,8 @@ class AugmentKitCLTools {
         switch option {
         case .serialize:
             serializeMDLAsset()
+        case .verify:
+            deserializeAndPrint()
         default:
             ConsoleIO.printUsage()
         }
@@ -77,22 +81,20 @@ class AugmentKitCLTools {
             return
         }
         
-        let metalAllocator = MTKMeshBufferAllocator(device: device)
-        
         var error: NSError?
         let myAsset: MDLAsset? = {
             if url.pathExtension == "scn" {
                 guard let scene = try? SCNScene(url: url, options: nil) else {
                     return nil
                 }
-                return MDLAsset(scnScene: scene, bufferAllocator: metalAllocator)
+                return MDLAsset(scnScene: scene)
             } else {
-                return MDLAsset(url: url, vertexDescriptor: AKSimpleModel.newVertexDescriptor(), bufferAllocator: metalAllocator, preserveTopology: false, error: &error)
+                return MDLAsset(url: url, vertexDescriptor: AKSimpleModel.newVertexDescriptor(), bufferAllocator: nil, preserveTopology: false, error: &error)
             }
         }()
         
         guard let asset = myAsset, error == nil else {
-            ConsoleIO.writeMessage("Model file not found.", to: .error)
+            ConsoleIO.writeMessage("Model file not found. \(url.absoluteString)", to: .error)
             return
         }
         
@@ -104,8 +106,55 @@ class AugmentKitCLTools {
             return
         }
         
-        NSKeyedArchiver.archiveRootObject(AKModelCodingWrapper(model: model), toFile: url.deletingLastPathComponent().appendingPathComponent("scene.dat").path)
+        NSKeyedArchiver.archiveRootObject(AKModelCodingWrapper(model: model), toFile: url.deletingLastPathComponent().appendingPathComponent("model.dat").path)
         
+    }
+    
+    fileprivate func deserializeAndPrint() {
+        
+        guard let url = url else {
+            ConsoleIO.printUsage()
+            return
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            ConsoleIO.writeMessage("File not found. \(url.absoluteString)", to: .error)
+            return
+        }
+        
+        if let wrapper = NSKeyedUnarchiver.unarchiveObject(with: data) as? AKModelCodingWrapper {
+            
+            guard let archivedModel = wrapper.model else {
+                ConsoleIO.writeMessage("AKModel is empty.", to: .error)
+                return
+            }
+            
+            ConsoleIO.writeMessage("indexBuffers: \(archivedModel.indexBuffers)", to: .standard)
+            ConsoleIO.writeMessage("instanceCount: \(archivedModel.instanceCount)", to: .standard)
+            ConsoleIO.writeMessage("jointRootID: \(archivedModel.jointRootID)", to: .standard)
+            ConsoleIO.writeMessage("localTransformAnimationIndices: \(archivedModel.localTransformAnimationIndices)", to: .standard)
+            ConsoleIO.writeMessage("localTransformAnimations: \(archivedModel.localTransformAnimations)", to: .standard)
+            ConsoleIO.writeMessage("localTransforms: \(archivedModel.localTransforms)", to: .standard)
+            ConsoleIO.writeMessage("meshes: \(archivedModel.meshes)", to: .standard)
+            ConsoleIO.writeMessage("meshNodeIndices: \(archivedModel.meshNodeIndices)", to: .standard)
+            ConsoleIO.writeMessage("meshSkinIndices: \(archivedModel.meshSkinIndices)", to: .standard)
+            ConsoleIO.writeMessage("nodeNames: \(archivedModel.nodeNames)", to: .standard)
+            ConsoleIO.writeMessage("parentIndices: \(archivedModel.parentIndices)", to: .standard)
+            ConsoleIO.writeMessage("sampleTimes: \(archivedModel.sampleTimes)", to: .standard)
+            ConsoleIO.writeMessage("skeletonAnimations: \(archivedModel.skeletonAnimations)", to: .standard)
+            ConsoleIO.writeMessage("skins: \(archivedModel.skins)", to: .standard)
+            ConsoleIO.writeMessage("texturePaths: \(archivedModel.texturePaths)", to: .standard)
+            ConsoleIO.writeMessage("vertexBuffers: \(archivedModel.vertexBuffers)", to: .standard)
+            ConsoleIO.writeMessage("vertexDescriptors: \(archivedModel.vertexDescriptors)", to: .standard)
+            ConsoleIO.writeMessage("worldTransformAnimationIndices: \(archivedModel.worldTransformAnimationIndices)", to: .standard)
+            ConsoleIO.writeMessage("worldTransformAnimations: \(archivedModel.worldTransformAnimations)", to: .standard)
+            ConsoleIO.writeMessage("worldTransforms: \(archivedModel.worldTransforms)", to: .standard)
+            
+        } else {
+            
+            ConsoleIO.writeMessage("Could not unarchive the model file.", to: .error)
+            
+        }
     }
     
 }
